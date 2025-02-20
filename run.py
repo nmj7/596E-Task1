8cb51a49598156499ba60e4e670ea03d1db34707
Nikitha <81354439+nmj7@users.noreply.github.com>
2025-02-20 01:33:17 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import onnx
 import onnxruntime as rt
-
+#program to check if onnx model was converted properly
 # Load the ONNX model
 model_path = "isolation_forest_model.onnx"
 onnx_model = onnx.load(model_path)
